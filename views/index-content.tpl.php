928c66ac8e10beeefad8deabdee056bf3470488d
oleg_remizov <job.rem1zoff@gmail.com>
2025-01-26 16:36:10 +0300

    CRUD finished

--- views/index-content.tpl.php
+++ views/index-content.tpl.php
@@ -2,27 +2,27 @@
     <?= $pagination ?>
     <table class="table table-hover">
         <thead class="table-dark">
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Name</th>
-                <th scope="col">Population</th>
-                <th scope="col">Actions</th>
-            </tr>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Population</th>
+            <th scope="col">Actions</th>
+        </tr>
         </thead>
         <tbody>
-            <?php foreach ($cities as $city): ?>
-                <tr id="city-<?= $city['id'] ?>">
-                    <th scope="row"><?= $city['id'] ?></th>
-                    <td><?= $city['name'] ?></td>
-                    <td><?= $city['population'] ?></td>
-                    <td>
-                        <button class="btn btn-info btn-edit" data-id="<?= $city['id'] ?>"
+        <?php foreach ($cities as $city): ?>
+            <tr id="city-<?= $city['id'] ?>">
+                <th scope="row"><?= $city['id'] ?></th>
+                <td class="name"><?= $city['name'] ?></td>
+                <td class="population"><?= $city['population'] ?></td>
+                <td>
+                    <button class="btn btn-info btn-edit" data-id="<?= $city['id'] ?>"
                             data-bs-toggle="modal" data-bs-target="#editCity">Edit
-                        </button>
-                        <button class="btn btn-danger btn-delete" data-id="<?= $city['id'] ?>">Delete</button>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
+                    </button>
+                    <button class="btn btn-danger btn-delete" data-id="<?= $city['id'] ?>">Delete</button>
+                </td>
+            </tr>
+        <?php endforeach; ?>
         </tbody>
     </table>
     <?= $pagination ?>
--- views/index.tpl.php
+++ views/index.tpl.php
@@ -1,87 +1,101 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>City</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
 </head>
-
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1 class="text-center h2 my-3">City</h1>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <button type="button" class="btn btn-primary rounded-0 btn-add" data-bs-toggle="modal" data-bs-target="#addCity">Add city</button>
-            </div>
-            <div class="table-responsive my-3">
-            <?= require_once "views/index-content.tpl.php" ?>
-            </div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center h2 my-3">City</h1>
         </div>
     </div>
 
-    <!-- Modal -->
-    <div class="modal fade" id="addCity" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Add city</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form method="post" id="addCityForm">
-                        <div class="modal-body">
-                            <div class="mb-3">
-                                <label for="addName" class="form-label">Name</label>
-                                <input type="text" name="name" class="form-control" id="addName" placeholder="City name">
-                            </div>
-                            <div class="mb-3">
-                                <label for="addPopulation" class="form-label">Population</label>
-                                <input type="number" name="population" class="form-control" id="addPopulation" placeholder="Population">
+    <div class="row">
+        <div class="col-12">
+            <button class="btn btn-primary rounded-0 btn-add" data-bs-toggle="modal" data-bs-target="#addCity">Add
+                city
+            </button>
+        </div>
 
-                                <input type="hidden" name="addCity">
-                            </div>
+        <div class="table-responsive my-3">
+            <?php require_once 'views/index-content.tpl.php' ?>
+        </div>
+    </div>
+</div>
+
+
+<!-- Modals -->
+<div class="modal fade" id="addCity" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Add city</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form method="post" id="addCityForm">
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="addName" class="form-label">Name</label>
+                            <input type="text" name="name" class="form-control" id="addName" placeholder="City name">
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary" id="btn-add-submit">Save</button>
+                        <div class="mb-3">
+                            <label for="addPopulation" class="form-label">Population</label>
+                            <input type="number" name="population" class="form-control" id="addPopulation"
+                                   placeholder="City population">
+                            <input type="hidden" name="addCity">
                         </div>
-                    </form>
-                </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary" id="btn-add-submit">Save</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-    <div class="modal fade" id="editCity" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Edit city</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
+</div>
+
+<div class="modal fade" id="editCity" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Edit city</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form method="post" id="editCityForm">
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="editName" class="form-label">Name</label>
+                            <input type="text" name="name" class="form-control" id="editName" placeholder="City name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="editPopulation" class="form-label">Population</label>
+                            <input type="number" name="population" class="form-control" id="editPopulation"
+                                   placeholder="City population">
+                            <input type="hidden" name="editCity">
+                            <input type="hidden" name="id" id="id">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary" id="btn-edit-submit">Save</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-    <!-- ./end modal -->
+</div>
 
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="assets/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="assets/main.js"></script>
 </body>
-
 </html>
